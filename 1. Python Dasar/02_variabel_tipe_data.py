16460a8fa2b3e66e5e4b0606667b2b5a71009275
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-01-25 11:55:25 +0700

    Variabel dan Tipe Data

---
+++ 1. Python Dasar/02_variabel_tipe_data.py
@@ -0,0 +1,24 @@
+# VARIABLE & TIPE DATA
+
+# integer (angka bulat)
+umur = 20
+
+# float (angka desimal)
+tinggi = 170.5
+
+# string (teks)
+nama = "Raffa"
+
+# boolean (true / false)
+is_student = True
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Mahasiswa:", is_student)
+
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(is_student))
+
