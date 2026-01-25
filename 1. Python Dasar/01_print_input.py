d6f97114e09849a2675ea4145eb9d08a18eb1475
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-01-25 11:48:27 +0700

    Fundamental Awal Cuy

---
+++ 1. Python Dasar/01_print_input.py
@@ -0,0 +1,9 @@
+# PRINT & INPUT
+# print() digunakan untuk menampilkan output ke layar
+
+print("Halo, ini program Python pertama gue")
+
+# input() digunakan untuk menerima input dari user
+nama = input("Masukkan nama kamu: ")
+
+print("Halo", nama, "selamat belajar Python!")
