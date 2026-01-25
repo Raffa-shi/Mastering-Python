6b2f8a9b8df6fddf09d447af365409430d7481de
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-01-25 13:16:34 +0700

    Penambahan List Tuple set

---
+++ 1. Python Dasar/05_list_tuple_set.py
@@ -0,0 +1,15 @@
+# LIST
+
+buah = ["apel", "pisang", "mangga"]
+buah.append("jeruk")  # menambah item
+
+print(buah)
+print(buah[0])
+
+# TUPLE (yang tidak bisa di ubah)
+angka = (1, 2, 3 )
+
+#SET (tidak berurutan, tidak ada duplikat)
+
+unik = {1, 2, 2, 3}
+print(unik)
