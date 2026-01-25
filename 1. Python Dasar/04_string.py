55a6f68ac6f98697764c1d45b3302f3ea18a6c2b
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-01-25 12:33:05 +0700

    add string dasar

---
+++ 1. Python Dasar/04_string.py
@@ -0,0 +1,12 @@
+# STRING
+
+nama = "data science"
+
+print(nama.upper())      # jadi huruf besar
+print(nama.lower())      # huruf kecil
+print(nama.capitalize()) # huruf pertama besar
+print(len(nama))         # panjang string
+
+# string formatting
+umur = 21
+print(f"Saya belajar {nama} umur {umur}")
