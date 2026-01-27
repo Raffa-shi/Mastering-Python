6152059f7564475ba9085f54865019e98a4aa3c7
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-01-27 21:08:18 +0700

    memahami konsep while looping

---
+++ 2. Control Flow/3_while_looping.py
@@ -0,0 +1,9 @@
+#white loop
+
+# ini digunakan untuk kondisi yang masih true
+
+count = 2 # inisialisasi variable angka yang di ambil dari nilai pertama misal 2 di mulai dari 2
+
+while count <= 10:
+    print("Perulangan ke-", count)
+    count += 1
