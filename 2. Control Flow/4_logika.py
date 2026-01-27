7f8fa3c81d4fb2669351dd31a1cfc4cd8e930b91
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-01-27 21:18:16 +0700

    Melatih Pengetahuan Flow Controll

---
+++ 2. Control Flow/4_logika.py
@@ -0,0 +1,9 @@
+# melatihan flow control
+
+nama = input("Masukkan nama Anda: ")
+nilai = int(input("Masukkan nilai Anda: "))
+
+if nilai >= 75:
+    print(nama, "Anda Lulus Dan Anda Jenius!")
+else:
+    print(nama, "Anda Tidak Lulus.")
