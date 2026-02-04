5675dbb067d3a474af88abc22d60b1038294c834
Raffa-shi <143743389+Raffa-shi@users.noreply.github.com>
2026-02-04 17:15:22 +0700

    Function python

---
+++ function/1_function_basic.py
@@ -0,0 +1,9 @@
+# FUNCTION BASIC
+# function digunakan untuk membungkus logic agar bisa dipakai ulang
+
+def sapa():
+    print("Halo, selamat belajar Python!")
+
+# panggil function
+sapa()
+sapa()
---
+++ function/readme.md
@@ -0,0 +1,3 @@
+# Running File 
+
+py "function/.py"
